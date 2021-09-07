5538aa27e9a6809f06667a7bfd9699338795c9db
FranciscoPS <francisco.ps1996@gmail.com>
2021-09-07 17:08:28 -0500

    Se obtienen las listas de usuarios

--- src/app/services/api/api.service.ts
+++ src/app/services/api/api.service.ts
@@ -2,18 +2,13 @@ import { Injectable } from '@angular/core';
 import { LoginInterface } from './../../models/login.interface';
 import { ResponseInterface } from './../../models/response.interface';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { listUsers, user } from './../../models/listUsers.interface';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-
-  // Credentials: 
-  // variables = {
-  //   "email": "eve.holt@reqres.in",
-  //   "password": "cityslicka"
-  // }
 
   private url: string = "https://reqres.in/api";
 
@@ -24,6 +19,11 @@ export class ApiService {
   loginByEmail(form: LoginInterface): Observable<ResponseInterface> {
     const direction = `${this.url}/login`;
     return this._http.post<ResponseInterface>(direction, form);
+  }
+
+  getAllUsers(page: number): Observable<listUsers> {
+    const direction = `${this.url}/users?page=${page}`;
+    return this._http.get<listUsers>(direction);
   }
 
 }
--- src/app/views/dashboard/dashboard.component.ts
+++ src/app/views/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../../services/api/api.service';
+import { user } from './../../models/listUsers.interface';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  public users: Array<user> = [];
+
+  constructor(private _apiService: ApiService) { }
 
   ngOnInit(): void {
+    this.getUsersByPage(1234);
+  }
+
+  getUsersByPage(page: number): void {
+    const response = this._apiService.getAllUsers(page);
+    response.subscribe(this.onSuccessGetUsers.bind(this), this.onError.bind(this));
+  }
+
+  onSuccessGetUsers(response): void {
+    this.users = response['data'];
+    console.log(this.users)
+  }
+
+  onError(error): void {
+    alert(error['error']);
   }
 
 }
