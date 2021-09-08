3baa0c30b0d4ae22820fc472dbc1445977c49264
FranciscoPS <francisco.ps1996@gmail.com>
2021-09-07 22:17:47 -0500

    Cambio de imgagen principal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,10 +14,10 @@ import { en_US } from 'ng-zorro-antd/i18n';
 import { registerLocaleData } from '@angular/common';
 import en from '@angular/common/locales/en';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NzTableModule } from 'ng-zorro-antd/table';
 import { NzPaginationModule } from 'ng-zorro-antd/pagination';
 import { NzSpinModule } from 'ng-zorro-antd/spin';
 import { NzNotificationModule } from 'ng-zorro-antd/notification';
+import { NzCardModule } from 'ng-zorro-antd/card';
 
 registerLocaleData(en);
 
@@ -36,10 +36,10 @@ registerLocaleData(en);
     FormsModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    NzTableModule,
     NzPaginationModule,
     NzSpinModule,
-    NzNotificationModule
+    NzNotificationModule,
+    NzCardModule
   ],
   providers: [{ provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent]
